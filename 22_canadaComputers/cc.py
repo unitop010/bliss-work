0e28e3746c0c01580cbc89566c36372c664f601f
Golden Unicorn <goldenunicorn531@gmail.com>
2024-06-10 12:57:31 +1000

    22_cc

---
+++ 22_canadaComputers/cc.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests, csv
+from urllib.parse import urljoin
+
+url = 'https://www.canadacomputers.com/location.php'
+print(f'~~~~~ URL: {url} ~~~~~')
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+# html_content = str(soup.prettify)
+# with open("webpage.html", "w", encoding="utf-8") as file:
+#   file.write(html_content)
+page_items = soup.findAll('div', class_='locwrapper')
+for page_item in page_items:
+  item_info = page_item.find(class_='order-1').findAll('a')
+  item_url = urljoin(url, item_info[0]['href'])
+  item_address = item_info[1].text.replace('\n','')
+  item_phone = item_info[2].text
+  item_email = item_info[3].text
+  output = []
+  output.append(item_url)
+  output.append(item_address)
+  output.append(item_phone)
+  output.append(item_email)
+
+  print(output)
+
+  open_out = open('output_cc.csv','a',newline="", encoding='utf-8')
+  file_o_csv = csv.writer(open_out, delimiter=',')
+  file_o_csv.writerow(output)
+  open_out.close()
+  # else:
+  #   continue
+print('#' * 20 + 'Finish' + '#' * 20)
