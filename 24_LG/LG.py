c5863f4714dcf684386419c1186681147ef9bfc8
Golden Unicorn <goldenunicorn531@gmail.com>
2024-06-17 04:37:28 +1000

    24_LG start

---
+++ 24_LG/LG.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.common.exceptions import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import ui
+from time import sleep
+from threading import Thread
+from openpyxl import Workbook
+import undetected_chromedriver as uc
+import json, csv, random, os, sys
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+states = ['ACTIVE', 'DISCONTINUED']
+for state in states:
+  print(f'*** {state} ***')
+  page_id = 1
+  while True:
+    base_url = f'https://www.lg.com/us/support/warranty-information#subcat=lap&cat=computers&page={page_id}&sort=0&side=b2c&ct=CT00000317&sp=&state='
+    driver.get(base_url + state)
+    try:
+      print(f'--- {page_id} page ---')
+      product_items = driver.find_elements(By.CLASS_NAME, 'box-result-item')
+    except:
+      break
+    for product_item in product_items:
+      # name
+      try:
+        product_name = product_item.find_element(By.CLASS_NAME, 'name')
+      except:
+        product_name = ''
+      # model number
+      try:
+        product_model = product_item.find_element(By.CLASS_NAME, 'model-number')
+      except:
+        product_model = ''
+      
+      output = [product_name, product_model]
+      
+      open_out = open('output_LG.csv','a',newline="", encoding='utf-8')
+      file_o_csv = csv.writer(open_out, delimiter=',')
+      file_o_csv.writerow(output)
+      open_out.close()
+    page_id += 1
+print('-' * 5 + 'Scraping Finished' + '-' * 5)
+driver.quit()
