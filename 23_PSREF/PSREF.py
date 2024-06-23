7212e9479cd84698bdaae5478d281cc0c350ca29
Golden Unicorn <goldenunicorn531@gmail.com>
2024-06-24 02:20:05 +1000

    24_PSREF remove keys

--- 23_PSREF/PSREF.py
+++ 23_PSREF/PSREF.py
@@ -7,7 +7,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.support import expected_conditions as EC
