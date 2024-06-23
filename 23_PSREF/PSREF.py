071d9bbab101dc5fb3ea74846677f4e861d95f57
Golden Unicorn <goldenunicorn531@gmail.com>
2024-06-24 02:29:32 +1000

    23_PSREF full script

--- 23_PSREF/PSREF.py
+++ 23_PSREF/PSREF.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import ui
 from time import sleep
-import random
+from threading import Thread
+from openpyxl import Workbook
+import undetected_chromedriver as uc
+import json, csv, random, os, sys
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
